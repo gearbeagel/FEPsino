881ad831aa7433bd27ee21a9064e9bcb3c9802fe
Vic K <kondratskayavictoria@gmail.com>
2025-03-16 18:29:08 +0200

    add: urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('api/user/', include('user.urls')),
+    # path('api/blackjack/', include('blackjack.urls')),
+    # path('api/dice/', include('dice.urls')),
+    # path('api/slots/', include('slots.urls')),
 ]
