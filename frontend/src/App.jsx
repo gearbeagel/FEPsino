477b114acad5976f8d09b461474523f66f05422c
Vic K <kondratskayavictoria@gmail.com>
2025-03-16 21:08:57 +0200

    fix: separated demo of teh page into components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
-import Homepage from './components/CasinoSite'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SlotsGame from "./components/games/SlotsGame"; // Example slot page component
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
   return (
-    <Homepage />
-  )
+    <Router>
+    <div className="h-screen overflow-hidden bg-black text-white flex flex-col">
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/slots" element={<SlotsGame />} />
+          <Route path="/blackjack" element={<h1>Blackjack</h1>} />
+          <Route path="/dice" element={<h1>Dice Games</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      <Footer />
+    </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <main className="flex-grow flex flex-col overflow-hidden bg-gradient-to-b from-gray-900 to-black py-12 min-h-screen">
+        <div className="container max-w-4xl mx-auto">
+      <div className="bg-gray-800 border border-yellow-400 rounded-lg p-6">
+        <h1 className="text-3xl font-bold mb-4">About FEPSino</h1>
+        <p className="text-lg text-gray-400">
+          FEPSino is a fictional online casino that offers a variety of games
+          for entertainment purposes. This project is a demo to showcase the use
+          of React, Tailwind CSS, and Lucide icons.
+        </p>
+      </div>
+      <div className="bg-gray-800 border border-yellow-400 rounded-lg p-6 mt-8">
+        <h1 className="text-3xl font-bold mb-4">Created by:</h1>
+        <ul className="text-lg text-gray-400">
+          <li>Vic. Kondratska: Team Lead, Frontend Developer</li>
+          <li>Nik. Pashchuk: Backend Developer</li>
+          <li>Vol. Demchyshyn: Backend Developer</li>
+          <li>Mar. Husak: Backend Developer</li>
+          <li>Dmy. Bilyk: Backend Developer</li>
+        </ul>
+      </div>
+    </div>
+    </main>
+  );
+}
