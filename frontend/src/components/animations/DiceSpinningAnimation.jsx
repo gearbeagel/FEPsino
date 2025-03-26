18f88a7240f11cc7a2a7fb0fa2887e9024e7744c
Vic K <kondratskayavictoria@gmail.com>
2025-03-26 20:58:17 +0200

    [feat] add dice animation

---
+++ frontend/src/components/animations/DiceSpinningAnimation.jsx
@@ -0,0 +1,19 @@
+const diceVariants = {
+    initial: {
+        rotate: 0,
+    },
+    animate: {
+        rotate: 360,
+        transition: {
+            duration: 0.5,
+        },
+    },
+    exit: {
+        rotate: 0,
+        transition: {
+            duration: 0.5,
+        },
+    },
+};
+
+export default diceVariants;
